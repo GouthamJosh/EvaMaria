68ed671b3840e969b1d1683aa51216fcb04ac06d
GouthamJosh <96323721+GouthamJosh@users.noreply.github.com>
2026-01-02 15:22:36 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,107 +1,139 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
 
-<b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
+    START_TXT = """👋 Hello {},
+My name is <a href=https://t.me/{}>{}</a>.
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+🎬 I can help you find and access movies easily!
+➕ Just add me to your group and enjoy the experience 😍
+"""
 
-<b>NOTE:</b>
-1. eva maria should have admin privillage.
-2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
+    HELP_TXT = """👋 Hey {},
+Here’s a quick guide to all my available commands and features.
+Use the buttons below to explore more 👇
+"""
 
-<b>Commands and Usage:</b>
-• /filter - <code>add a filter in chat</code>
-• /filters - <code>list all the filters of a chat</code>
-• /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
-    BUTTON_TXT = """Help: <b>Buttons</b>
+    ABOUT_TXT = """✨ <b>About Me</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+✯ <b>Name:</b> {}
+✯ <b>Creator:</b> <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
+✯ <b>Library:</b> Pyrogram
+✯ <b>Language:</b> Python 3
+✯ <b>Database:</b> MongoDB
+✯ <b>Server:</b> Heroku
+✯ <b>Build Status:</b> v1.0.1 [BETA]
+"""
 
-<b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
+    SOURCE_TXT = """<b>📌 NOTE:</b>
+• Eva Maria is an open-source project.
+• Source Code: https://github.com/EvamariaTG/EvaMaria
 
-<b>URL buttons:</b>
+<b>👨‍💻 Developers:</b>
+• <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
+"""
+
+    MANUELFILTER_TXT = """<b>📂 Filters Help</b>
+
+Filters allow admins to set automated replies for specific keywords.
+Whenever a keyword is detected, Eva Maria will respond automatically.
+
+<b>⚠️ Notes:</b>
+1. Eva Maria must have admin privileges.
+2. Only admins can add filters.
+3. Alert buttons are limited to 64 characters.
+
+<b>🛠 Commands:</b>
+• /filter – <code>Add a filter</code>
+• /filters – <code>List all filters</code>
+• /del – <code>Delete a specific filter</code>
+• /delall – <code>Delete all filters (Owner only)</code>
+"""
+
+    BUTTON_TXT = """<b>🔘 Buttons Help</b>
+
+Eva Maria supports both URL buttons and alert buttons.
+
+<b>⚠️ Notes:</b>
+1. Buttons cannot be sent without content.
+2. All Telegram media types are supported.
+3. Buttons must follow correct Markdown format.
+
+<b>🌐 URL Button:</b>
 <code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
 
-<b>Alert buttons:</b>
-<code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
-
-<b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains camrips, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
-    CONNECTION_TXT = """Help: <b>Connections</b>
-
-- Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
-
-<b>NOTE:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
-
-<b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
-• /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
-    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
-
-<b>NOTE:</b>
-these are the extra features of Eva Maria
-
-<b>Commands and Usage:</b>
-• /id - <code>get id of a specified user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
-
-<b>NOTE:</b>
-This module only works for my admins
-
-<b>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
-• /delete - <code>to delete a specific file from db.</code>
-• /users - <code>to get list of my users and ids.</code>
-• /chats - <code>to get list of the my chats and ids </code>
-• /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
-• /ban  - <code>to ban a user.</code>
-• /unban  - <code>to unban a user.</code>
-• /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
-    LOG_TEXT_G = """#NewGroup
-Group = {}(<code>{}</code>)
-Total Members = <code>{}</code>
-Added By - {}
+<b>🚨 Alert Button:</b>
+<code>[Button Text](buttonalert:This is an alert message)</code>
 """
-    LOG_TEXT_P = """#NewUser
-ID - <code>{}</code>
-Name - {}
+
+    AUTOFILTER_TXT = """<b>🤖 Auto Filter Help</b>
+
+<b>⚠️ Notes:</b>
+1. Make me admin if your channel is private.
+2. Do not add camrips, adult, or fake files.
+3. Forward the latest message from your channel with quotes.
+
+I will automatically index all files into my database.
+"""
+
+    CONNECTION_TXT = """<b>🔗 Connections Help</b>
+
+Connections link your group with PM for easier filter management
+and help reduce group spam.
+
+<b>⚠️ Notes:</b>
+1. Only admins can create connections.
+2. Send <code>/connect</code> in the group.
+
+<b>🛠 Commands:</b>
+• /connect – <code>Connect a chat to PM</code>
+• /disconnect – <code>Disconnect from a chat</code>
+• /connections – <code>List all connected chats</code>
+"""
+
+    EXTRAMOD_TXT = """<b>➕ Extra Modules</b>
+
+Additional useful features available in Eva Maria.
+
+<b>🛠 Commands:</b>
+• /id – <code>Get user ID</code>
+• /info – <code>Get user information</code>
+• /imdb – <code>Fetch movie details from IMDb</code>
+• /search – <code>Search movie info from multiple sources</code>
+"""
+
+    ADMIN_TXT = """<b>🛡 Admin Commands</b>
+
+⚠️ These commands are restricted to bot admins only.
+
+<b>🛠 Commands:</b>
+• /logs – <code>View recent errors</code>
+• /stats – <code>Database statistics</code>
+• /delete – <code>Delete a file from DB</code>
+• /users – <code>List all users</code>
+• /chats – <code>List all chats</code>
+• /leave – <code>Leave a chat</code>
+• /disable – <code>Disable a chat</code>
+• /ban – <code>Ban a user</code>
+• /unban – <code>Unban a user</code>
+• /channel – <code>List connected channels</code>
+• /broadcast – <code>Send message to all users</code>
+"""
+
+    STATUS_TXT = """📊 <b>Bot Statistics</b>
+
+★ Total Files: <code>{}</code>
+★ Total Users: <code>{}</code>
+★ Total Chats: <code>{}</code>
+★ Used Storage: <code>{}</code> MiB
+★ Free Storage: <code>{}</code> MiB
+"""
+
+    LOG_TEXT_G = """#NewGroupAdded
+🏷 Group: {} (<code>{}</code>)
+👥 Members: <code>{}</code>
+➕ Added By: {}
+"""
+
+    LOG_TEXT_P = """#NewUserStarted
+🆔 ID: <code>{}</code>
+👤 Name: {}
 """
